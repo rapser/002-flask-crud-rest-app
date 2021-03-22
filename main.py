6b51db367d3dc0df9e809cafacbdbd5d8b6a62cd
Miguel Angel Tomairo <matomairo@gmail.com>
2021-03-21 22:06:47 -0500

    refactor connection

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 from flask import Flask, jsonify, request, Response
 from bson import json_util
 from bson.objectid import ObjectId
+from src.connection import *
 
+app = Flask(__name__)
 import pymongo
 import certifi
 
-app = Flask(__name__)
-
-client = pymongo.MongoClient("mongodb+srv://rapser:rapser2018@cluster0.5zuzx.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where())
+client = obtener_bd()
 db = client['tienda']
-print(client.list_database_names())
+collection = db.productos
+#print(client.list_database_names())
 
 @app.route('/products/<string:id>', methods=['GET'])
 def getProduct(id):
 
-    product = db.productos.find_one({"_id": ObjectId(id)})
+    product = collection.find_one({"_id": ObjectId(id)})
     response = json_util.dumps(product)
     return Response(response, mimetype='application/json')
 
 @app.route('/products', methods=['GET'])
 def getProducts():
-    products = db.productos.find()
+    products = collection.find()
     res = json_util.dumps(products)
     return Response(res, mimetype='application/json')
 
@@ -32,7 +33,7 @@ def addProduct():
     quantity = request.json['quantity']
     
     if name and price and quantity:
-        db.productos.insert_one({
+        collection.insert_one({
             'name': name,
             'price': price,
             'quantity': quantity
@@ -43,7 +44,7 @@ def addProduct():
 
 @app.route('/products/<string:id>', methods=['DELETE'])
 def deleteProduct(id):
-    productFound = db.productos.delete_one({"_id": ObjectId(id)})
+    productFound = collection.delete_one({"_id": ObjectId(id)})
     response = json_util.dumps({"message": "eliminado"})
     return Response(response, mimetype='application/json')
 
@@ -54,7 +55,7 @@ def editProduct(id):
     price = request.json['price']
     quantity = request.json['quantity']
 
-    db.productos.update_one({"_id": ObjectId(id)}, {"$set": {
+    collection.update_one({"_id": ObjectId(id)}, {"$set": {
         'name': name,
         'price': price,
         'quantity': quantity
